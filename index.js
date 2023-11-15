9651e86a978aa000cbcb02eda098d3730259a99d
LocTM <tranminhloc.anhk@gmail.com>
2023-11-15 21:08:11 +0700

    BT2

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 // for (let i = 0; i < 6; i++){
 //     document.write(`The number is ${i} <br>`)
 // }
+
+let i = 1;
+let num;
+let total= 0;
+num = prompt("nhap vao day 1 so bat ki")
+for (; i <= num; i++ ){
+    total += i;
+}
+alert(`Tong cac so tu 0 den ${num} la: ${total}`);
